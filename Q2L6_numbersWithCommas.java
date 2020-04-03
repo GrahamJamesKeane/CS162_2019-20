bec99355ce30896db4a273a1610c6dfe326d209c
Graham Keane <40239414+GrahamJamesKeane@users.noreply.github.com>
2020-04-03 21:58:46 +0100

    Add files via upload

---
+++ Q2L6_numbersWithCommas.java
@@ -0,0 +1,66 @@
+/**
+*	Lab 6:	Question 2
+*	Description
+*	Write a program which contains a method called commaNumbers. This method takes one input to 
+*	represent a number (an integer). The method should return a String to represent the same 
+*	number with appropriate commas in place.
+*
+*	Sample Input 1:		1000
+*	Sample Output 1:	1,000
+*	Sample Input 2:		100000
+*	Sample Output 2:	100,000
+*	Sample Input 3:		20
+*	Sample Output 3:	20
+*
+*	Algorithm:
+*	Step 1:	Take input from user
+*	Step 2: Pass input to commaNumers() method
+*			A)	We create a new String, called output to contain the modified input String
+*			B)	Check String is longer than three characters. We won't place a comma in a number less than 1000
+*			C)	With a for-loop we iterate from the end of the String towards the start. 
+*				i)		If we find that int pos is not zero and is divisble by 3 then we add a comma between the character at
+*				    	a[i] and the remainder of the String.
+*				ii)		Else we append the character a[i] without a comma to the remainder of the String.
+*				iii) 	We update pos.
+*			D)	Return output to main
+*	Step 3:	Print the result
+*/
+
+import java.util.Scanner;
+
+public class Q2L6_numbersWithCommas
+{
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		System.out.println(commaNumbers(input));
+	}	//End main
+	
+	public static String commaNumbers(String input)
+	{
+		String output = new String("");
+		if(input.length() <= 3)
+		{
+			return input;
+		}
+		else
+		{
+			String[] a = input.split("");
+			int pos = 0;
+			for(int i = a.length - 1; i >=0; i--)
+			{
+				if(pos != 0 && pos % 3 == 0)
+				{
+					output = a[i] + "," + output;
+				}
+				else
+				{
+					output = a[i] + output;
+				}
+				pos++;
+			}	//End for
+		}	//End outer else
+			return output;
+	}	//End commaNumbers()
+}	//End class
---
+++ Q3L6_mexicanWave.java
@@ -0,0 +1,110 @@
+/**
+*	Lab 6:	Question 3
+*
+*	Description:
+*	The mexican wave is a famous motion that is often perfomed by a crowd at a sporting event. 
+*	The motion involves an effect resembling a moving wave produced by successive sections of 
+*	the crowd in a stadium standing up, raising their arms, lowering them, and sitting down again.
+*	Your task for this program is to simulate a "mexican wave" on a String. Within your program, 
+*	create a method called wave which takes one String as an input. This method should return an 
+*	array of stages in the wave.
+*
+*	For example, with the word "hello", your mexican wave will be ["Hello", "hEllo", "heLlo", helLo", hellO"]. 
+*	When your array has been created, print out its contents one word per line.
+*
+*	NOTE: Spaces are ignored in the mexican wave.
+*
+*	Sample Input 1:		hello
+*	Sample Output 1:	Hello
+*						hEllo
+*    					heLlo
+*   					helLo
+*   					hellO
+*
+*	Sample Input 2:		hello world
+*	Sample Output 2:	Hello world
+*						hEllo world
+*						heLlo world
+*						helLo world
+*						hellO world
+*						hello World
+*						hello wOrld
+*						hello woRld
+*						hello worLd
+*						hello worlD
+*
+*	Algorithm:
+*	Step 1:	Take input from user
+*
+*	Step 2: Pass input to wave() method which returns a String array to output
+*			A)	Make sure string is lowercase
+*			B)	Create String array to store each word in the wave pattern
+*			C)	Within a for-loop we pass input to a String temp each iteration
+*				i)		Moving from left to right we select char at position i
+*				ii)		Assuming the characters are letters and whitespace, if charAt(i) is not a space we add it's uppercase variant to a second char variable
+*				iii)	We construct a temp String according to the value of i. 
+*						1)	At position 0 we append c2 at the left and the remaining string to the right. 
+*						2)	At positions between the first and last index we append in the middle.
+*						3)	At the last index we append to right.
+*				iv)		Finally, we send the temp String to output[i] and repeat.
+*			D)	Output is returned to main
+*
+*	Step 3:	We print ouput using a for-each loop
+*						
+*
+**/
+
+import java.util.Scanner;
+
+public class Q3L6_mexicanWave
+{
+	public static void main(String[] args)
+	{	
+		Scanner scan = new Scanner(System.in);	//Step 1:
+		String input = scan.nextLine();
+		scan.close();
+		String[] output = wave(input);	//Step 2:
+		
+		for(String y: output)
+		{	//Step3:
+			System.out.println(y);
+		}
+	}	//End main
+	
+	
+	public static String[] wave(String input)
+	{
+		input = input.toLowerCase();	//A)
+		int size = input.length();
+		String[] output = new String[size];	//B)
+		
+		for(int i = 0; i < size; i++)
+		{	//C)
+			String temp = input;
+			char c1 = temp.charAt(i);	//i)
+			int x = 0;
+			char c2 = ' ';
+			
+			if(c1 != ' ')
+			{	//ii)
+				x = c1 - 32;
+				c2 = (char)x;
+			}
+			
+			if(i == 0)	//iii)
+			{	//1)
+				temp = c2 + temp.substring(1);
+			}
+			else if(i > 0 && i < size - 1)
+			{	//2)
+				temp = temp.substring(0, i) + c2 + temp.substring(i + 1);
+			}
+			else
+			{	//3)
+				temp = temp.substring(0, i) + c2;
+			}
+			output[i] = temp;	//iv)
+		}	//End for-loop
+		return output;	//D)
+	}	//End wave()
+}	//End class
